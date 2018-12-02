6526042d8f0748589f6f302b741ed7b4adc33709
Tonyjlum <tonyjlum@gmail.com>
2018-12-02 18:07:21 -0500

    Student to SQL through ORM

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,57 @@
 require_relative "../config/environment.rb"
-
+require 'pry'
 class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :name, :grade, :id
 
+
+  def initialize(name, grade, id = nil)
+    @name, @grade, @id = name, grade, id
+  end
+
+  def self.create_table
+  sql =" CREATE TABLE IF NOT EXISTS students(id INTEGER PRIMARY KEY,  name TEXT, grade INTEGER)"
+  DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = "INSERT INTO students(name, grade) VALUES (?,?)"
+      DB[:conn].execute(sql, self.name, self.grade)
+      sql_id = "SELECT last_insert_rowid() FROM students"
+      @id = DB[:conn].execute(sql_id).first.first
+    end
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.create(name, grade)
+    new_student = self.new(name, grade)
+    new_student.save
+    new_student
+  end
+
+  def self.new_from_db(row)
+    self.new(row[1], row[2], row[0])
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
 end
